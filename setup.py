95b985a75b7f70d41a1ca970a8f3523846ed981e
Lars Walther <lwalther@novareto.de>
2025-06-24 16:29:34 +0200

    Back to development: 1.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='nva.keycloak',
-    version='1.0',
+    version='1.1.dev0',
     description="Keycloak SPI for Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
