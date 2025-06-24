985bcc9cb0eeaa7817e71797b0bcce9893a3c708
Lars Walther <lwalther@novareto.de>
2025-06-24 16:29:24 +0200

    Preparing release 1.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,8 +2,8 @@ Changelog
 =========
 
 
-1.0a1 (unreleased)
-------------------
+1.0 (2025-06-24)
+----------------
 
 - Initial release.
   [lwalther]
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='nva.keycloak',
-    version='1.0a1',
+    version='1.0',
     description="Keycloak SPI for Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
